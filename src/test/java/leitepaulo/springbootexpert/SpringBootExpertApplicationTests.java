f4fc64437b0f5fb4b5c133e7ac7a18f409b3a6e7
Paulo Leite <pauloferreira.sistemas@gmail.com>
2022-03-10 09:09:34 -0300

    first commit

---
+++ src/test/java/leitepaulo/springbootexpert/SpringBootExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package leitepaulo.springbootexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
